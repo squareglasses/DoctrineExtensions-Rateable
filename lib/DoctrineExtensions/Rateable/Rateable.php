7e2521e3c11448dfc12d0fac5f0c8d0170ba6969
Bat <contact@graindeweb.fr>
2012-02-08 19:18:03 +0100

    change average method name and add some comments

--- lib/DoctrineExtensions/Rateable/Rateable.php
+++ lib/DoctrineExtensions/Rateable/Rateable.php
@@ -14,17 +14,52 @@ namespace DoctrineExtensions\Rateable;
  * Rateable is the interface that rateable resource classes must implement.
  *
  * @author Fabien Pennequin <fabien@pennequin.me>
+ * @author Bat <contact@graindeweb.fr>
  */
 interface Rateable
 {
+    /**
+     * Get rateable identifier, typically id (getId())
+     * return mixed
+     */
     function getRateableId();
+    
+    /**
+     * Get rateable Type, typically class name
+     * @return string
+     */
     function getRateableType();
 
+    
+    /**
+     * Return number of votes
+     * @return integer
+     */
     function getRatingVotes();
+    
+    /**
+     * Set number of votes
+     * @param integer $number
+     */
     function setRatingVotes($number);
 
+    
+    /**
+     * Get rateable object score (total)
+     * @return integer
+     */
     function getRatingTotal();
+    
+    /**
+     * Set rateable object score (total)
+     * @param integer $number
+     */
     function setRatingTotal($number);
     
-    function getAverageRates();
+    
+    /**
+     * Compute the average score of a resource
+     * @return integer
+     */
+    function getAverageScore();
 }
